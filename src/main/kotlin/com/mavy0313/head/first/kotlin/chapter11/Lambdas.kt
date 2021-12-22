9eef6d36f5fdcb9f08815e6337b18a79df907fb0
Max <vypoff@gmail.com>
2021-12-22 15:17:17 +0300

    feat: lambda as parameter

--- src/main/kotlin/com/mavy0313/head/first/kotlin/chapter11/Lambdas.kt
+++ src/main/kotlin/com/mavy0313/head/first/kotlin/chapter11/Lambdas.kt
@@ -1,5 +1,18 @@
 package com.mavy0313.head.first.kotlin.chapter11
 
+fun convert(x: Double,
+            converter: (Double) -> Double): Double {
+    val result = converter(x)
+    println("$x is converted to $result")
+    return result
+}
+
+fun convertFive(converter: (Int) -> Double): Double {
+    val result = converter(5)
+    println("5 is converted to $result")
+    return result
+}
+
 fun main() {
     var addFive = { x: Int -> x + 5}
     println("Pass 6 to addFive: ${addFive(6)}")
@@ -16,4 +29,7 @@ fun main() {
 
     val myLambda: () -> Unit = { println("Hi!") }
     myLambda()
+
+    convert(20.0) { it * 1.8 + 32 }
+    convertFive { it * 1.8 + 32 }
 }
