4b6b5be3ebfc542f83f834e0f379c3d9abdd6746
Max <vypoff@gmail.com>
2021-12-23 11:10:09 +0300

    feat: combine lambdas

--- src/main/kotlin/com/mavy0313/head/first/kotlin/chapter11/Lambdas.kt
+++ src/main/kotlin/com/mavy0313/head/first/kotlin/chapter11/Lambdas.kt
@@ -1,7 +1,9 @@
 package com.mavy0313.head.first.kotlin.chapter11
 
+typealias DoubleConversion = (Double) -> Double
+
 fun convert(x: Double,
-            converter: (Double) -> Double): Double {
+            converter: DoubleConversion): Double {
     val result = converter(x)
     println("$x is converted to $result")
     return result
@@ -13,8 +15,30 @@ fun convertFive(converter: (Int) -> Double): Double {
     return result
 }
 
+fun getConversionLambda(str: String): DoubleConversion {
+    when (str) {
+        "CentigradeToFahrenheit" -> {
+            return { it * 1.8 + 32 }
+        }
+        "KgsToPounds" -> {
+            return { it * 2.204623}
+        }
+        "PoundsToUSTons" -> {
+            return { it / 2000.0 }
+        }
+        else -> {
+            return { it }
+        }
+    }
+}
+
+fun combine(lambda1: DoubleConversion,
+            lambda2: DoubleConversion): DoubleConversion {
+    return { x: Double -> lambda2(lambda1(x)) }
+}
+
 fun main() {
-    var addFive = { x: Int -> x + 5}
+    var addFive = { x: Int -> x + 5 }
     println("Pass 6 to addFive: ${addFive(6)}")
 
     var addInts = { x: Int, y: Int -> x + y }
@@ -24,7 +48,7 @@ fun main() {
     var intLambda: (Int, Int) -> Int = { x, y -> x * y }
     println("Pass 10, 11 to intLambda: ${intLambda(10, 11)}")
 
-    val addSeven: (Int) -> Int = { it + 7}
+    val addSeven: (Int) -> Int = { it + 7 }
     println("Pass 12 to addSeven: ${addSeven(12)}")
 
     val myLambda: () -> Unit = { println("Hi!") }
@@ -32,4 +56,12 @@ fun main() {
 
     convert(20.0) { it * 1.8 + 32 }
     convertFive { it * 1.8 + 32 }
+
+    println("Convert 2.5kg to Pounds: ${getConversionLambda("KgsToPounds")(2.5)}")
+    val kgsToPoundsLambda = getConversionLambda("KgsToPounds")
+    val poundsToUSTonsLambda = getConversionLambda("PoundsToUSTons")
+    val kgsToUSTonsLambda = combine(kgsToPoundsLambda, poundsToUSTonsLambda)
+
+    val value = 17.4
+    println("$value kgs is ${convert(value, kgsToUSTonsLambda)} US tons")
 }
